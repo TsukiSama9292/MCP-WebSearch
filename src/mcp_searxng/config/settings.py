d60b03b2886688eb95046177005e9cc882623db1
TsukiSama9292 <a0985821880@gmail.com>
2025-08-05 10:16:01 +0000

    [update] Docker Compose 啟動

--- src/mcp_searxng/config/settings.py
+++ src/mcp_searxng/config/settings.py
@@ -2,6 +2,6 @@ from pydantic.v1 import BaseSettings
 
 class Settings(BaseSettings):
     """Settings for the MCP SearxNG integration."""
-    searxng_url: str = "http://192.168.200.102:8080"  # Default SearxNG URL
+    searxng_url: str = "http://searxng:8080"
 
 setting = Settings()
